e9c6b0adc1f51bdee88cc9781216b3ea532bb29d
Bullets-jyr <jyr@bullets.co.kr>
2023-09-21 19:56:25 +0900

    Family Modifier 실습

---
+++ lib/screen/family_modifier_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:statement_river/layout/default_layout.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:statement_river/riverpod/family_modifier_provider.dart';
+
+class FamilyModifierScreen extends ConsumerWidget {
+  const FamilyModifierScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // family_modifier_provider에 있는 data값에 해당되는 파라미터를 넣어줘야합니다.
+    final state = ref.watch(familyModifierProvider(3));
+    return DefaultLayout(
+      title: 'FamilyModifierScreen',
+      body: Center(
+        child: state.when(
+          data: (data) => Text(
+            data.toString(),
+          ),
+          error: (err, stack) => Text(
+            err.toString(),
+          ),
+          loading: () => CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:statement_river/layout/default_layout.dart';
+import 'package:statement_river/screen/family_modifier_screen.dart';
 import 'package:statement_river/screen/future_provider_screen.dart';
 import 'package:statement_river/screen/state_notifier_provider_screen.dart';
 import 'package:statement_river/screen/state_provider_screen.dart';
@@ -53,6 +54,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('StreamProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => FamilyModifierScreen(),
+                ),
+              );
+            },
+            child: Text('FamilyModifierScreen'),
           ),
         ],
       ),
