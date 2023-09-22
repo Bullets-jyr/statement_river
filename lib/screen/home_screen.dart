988609178f05cd7423da7961cc2bdfe405b24f1f
Bullets-jyr <jyr@bullets.co.kr>
2023-09-22 15:03:20 +0900

    Listen 함수 실습

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:statement_river/layout/default_layout.dart';
 import 'package:statement_river/screen/auto_dispose_modifier_screen.dart';
 import 'package:statement_river/screen/family_modifier_screen.dart';
 import 'package:statement_river/screen/future_provider_screen.dart';
+import 'package:statement_river/screen/listen_provider_screen.dart';
 import 'package:statement_river/screen/state_notifier_provider_screen.dart';
 import 'package:statement_river/screen/state_provider_screen.dart';
 import 'package:statement_river/screen/stream_provider_screen.dart';
@@ -75,6 +76,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('AutoDisposeModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ListenProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ListenProviderScreen'),
           ),
         ],
       ),
