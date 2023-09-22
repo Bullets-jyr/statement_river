574d6619b064433530cff9815038ae6d2d46b5f2
Bullets-jyr <jyr@bullets.co.kr>
2023-09-22 17:32:17 +0900

    ProviderObserver 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:statement_river/riverpod/provider_observer.dart';
 import 'package:statement_river/screen/home_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     ProviderScope(
+      observers: [
+        Logger(),
+      ],
       child: MaterialApp(
         home: HomeScreen(),
       ),
